8e748edc7e41bedf9d99a2618b1fb28fc8fca54c
Aewyn <Aewyn@outlook.com>
2022-05-19 10:48:12 +0200

    Eerste commit van de Spring theorie. Basic files.

---
+++ src/main/java/com/example/luigi/LuigiApplication.java
@@ -0,0 +1,13 @@
+package com.example.luigi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuigiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuigiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/luigi/LuigiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.luigi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuigiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
