d5dc497dead5a41ce8cff7b417d64e677c7a3b50
Aewyn <aewyn@outlook.com>
2022-06-02 10:58:57 +0200

    Added RequestMapping

--- src/main/java/com/example/luigi/controllers/PizzaController.java
+++ src/main/java/com/example/luigi/controllers/PizzaController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.math.BigDecimal;
@@ -12,25 +13,26 @@ import java.util.Arrays;
 import java.util.Optional;
 
 @Controller
+@RequestMapping("pizzas")
 public class PizzaController {
     private final Pizza[] allePizzas = {
             new Pizza(1, "Prosciutto", BigDecimal.valueOf(4), true),
             new Pizza(2, "Margherita", BigDecimal.valueOf(5), false),
             new Pizza(3, "Calzone", BigDecimal.valueOf(4), false)
     };
-    @GetMapping("/pizzas")
+    @GetMapping
     public ModelAndView findAll(){
         return new ModelAndView("pizzas", "allePizzas", allePizzas);
     }
 
-    private Optional<Pizza> findByIdHelper(long id){
-        return Arrays.stream(allePizzas).filter(pizza->pizza.getId()==id).findFirst();
-    }
-
-    @GetMapping("/pizzas/{id}")
+    @GetMapping("{id}")
     public ModelAndView findById(@PathVariable long id){
         var modelAndView = new ModelAndView("pizza");
         findByIdHelper(id).ifPresent(gevondenPizza->modelAndView.addObject("pizza", gevondenPizza));
         return modelAndView;
+    }
+
+    private Optional<Pizza> findByIdHelper(long id){
+        return Arrays.stream(allePizzas).filter(pizza->pizza.getId()==id).findFirst();
     }
 }
