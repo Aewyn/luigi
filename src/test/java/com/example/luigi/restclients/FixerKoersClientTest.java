a2ac4b52182130deccf416db366fbdd94aed2aa4
Aewyn <aewyn@outlook.com>
2022-06-14 15:51:07 +0200

    Removed unused imports.

--- src/test/java/com/example/luigi/restclients/FixerKoersClientTest.java
+++ src/test/java/com/example/luigi/restclients/FixerKoersClientTest.java
@@ -1,6 +1,5 @@
 package com.example.luigi.restclients;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.context.annotation.Import;
