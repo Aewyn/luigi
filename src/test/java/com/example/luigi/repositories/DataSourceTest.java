bfa8b693ec28e5ae5d5f7c7463be4ed40259219d
Aewyn <aewyn@outlook.com>
2022-06-20 16:33:48 +0200

    Added variables in application.properties for jdbc connection. Created a test DataSourceTest to see if the connection is succesful.

---
+++ src/test/java/com/example/luigi/repositories/DataSourceTest.java
@@ -0,0 +1,23 @@
+package com.example.luigi.repositories;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@JdbcTest
+public class DataSourceTest {
+    private final DataSource dataSource;
+    DataSourceTest(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+    @Test
+    void getConnection() throws SQLException{
+        try(var connection = dataSource.getConnection()){
+            assertThat(connection.getCatalog()).isEqualTo("luigi");
+        }
+    }
+}
