c43b88dee45bbbc9c03344c3f3f4a88dddf29b98
Aewyn <aewyn@outlook.com>
2022-06-25 17:09:02 +0200

    Started addding @Transactional functionality

---
+++ src/test/java/com/example/luigi/services/PizzaServiceTest.java
@@ -0,0 +1,34 @@
+package com.example.luigi.services;
+
+import com.example.luigi.domain.Pizza;
+import com.example.luigi.repositories.PizzaRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PizzaServiceTest {
+    @Mock
+    private PizzaRepository repository;
+    private PizzaService service;
+
+    @BeforeEach
+    void beforeEach(){
+        service = new PizzaService(repository);
+    }
+    @Test
+    void create(){
+        var pizza = new Pizza(0, "test", BigDecimal.TEN, false);
+        when(repository.create(pizza)).thenReturn(1L);
+        assertThat(service.create(pizza)).isEqualTo(1L);
+        verify(repository).create(pizza);
+    }
+}
